9c5c22d0b38d5475c3ad00f47d0745ae874b2ba3
klam0021 <klam0021@student.monash.edu>
2024-07-31 13:58:01 +1000

    creating all needed files

--- app/src/main/java/com/example/myapplication/ownerReviewRespondPage.java
+++ app/src/main/java/com/example/myapplication/ownerReviewRespondPage.java
@@ -8,14 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class registrationPage extends AppCompatActivity {
+public class ownerReviewRespondPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_registration_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.usernameInput), (v, insets) -> {
+        setContentView(R.layout.activity_owner_review_respond_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
