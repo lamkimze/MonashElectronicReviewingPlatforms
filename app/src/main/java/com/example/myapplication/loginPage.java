2bb3c4915d6ab3a386d098ebc4980aea10f5462b
klam0021 <klam0021@student.monash.edu>
2024-08-01 12:17:56 +1000

    adding logic on sign up page and ui, adding ui on log in page

--- app/src/main/java/com/example/myapplication/loginPage.java
+++ app/src/main/java/com/example/myapplication/loginPage.java
@@ -1,24 +1,48 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class loginPage extends AppCompatActivity {
+    EditText etUserName, etPassword;
+    String registeredUsername, registeredPassword, stringUserName, stringPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.usernameInput), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        restoredUserData();
+        etUserName = findViewById(R.id.loginUsernameInput);
+        etPassword = findViewById(R.id.loginPasswordInput);
+    }
+
+    public void onClickLogin(View view){
+        stringUserName = etUserName.getText().toString();
+        stringPassword = etPassword.getText().toString();
+        if(registeredUsername.equals(stringUserName) && registeredPassword.equals(stringPassword)){
+            Toast.makeText(this, "Login Successful !!", Toast.LENGTH_LONG).show();
+            Intent loginIntent = new Intent(this, dashboardPage.class);
+            startActivity(loginIntent);
+        }
+    }
+
+    public void onClickRegister(View view){
+        Intent registerIntent = new Intent(this, userRegistrationPage.class);
+        startActivity(registerIntent);
+    }
+
+    public void restoredUserData(){
+        SharedPreferences sharedPreferences = getSharedPreferences("USER_DATAFILE", MODE_PRIVATE);
+        registeredUsername = sharedPreferences.getString("USERNAME", "DEFAULT_VALUE");
+        registeredPassword = sharedPreferences.getString("PASSWORD", "DEFAULT_VALUE");
+
     }
 }
--- app/src/main/java/com/example/myapplication/userRegistrationPage.java
+++ app/src/main/java/com/example/myapplication/userRegistrationPage.java
@@ -1,6 +1,15 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +17,93 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class userRegistrationPage extends AppCompatActivity {
+
+    EditText etFirstName, etLastName, etUserName, etPassword, etConPassword, etEmail;
+    String stringFirstName, stringLastName, stringUserName, stringPassword, stringConPassword, stringEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registration_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.usernameInput), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        etFirstName = findViewById(R.id.regFirstName);
+        etLastName = findViewById(R.id.regLastName);
+        etUserName = findViewById(R.id.regUsername);
+        etPassword = findViewById(R.id.regPassword);
+        etConPassword = findViewById(R.id.regConPassword);
+        etEmail = findViewById(R.id.regEmail);
+
+
+    }
+
+    public void onRadioButtonClicked(View view){
+        String str = "";
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        if(radioGroup.getCheckedRadioButtonId() == R.id.personalRadioBtn){
+            str = "personal account";
+        }
+        else if(radioGroup.getCheckedRadioButtonId() == R.id.businessRadioBtn){
+            str = "business account";
+        }
+        Toast.makeText(this, str, Toast.LENGTH_LONG).show();
+    }
+
+
+    public void onClickSubmit(View view){
+        stringFirstName = etFirstName.getText().toString();
+        stringLastName = etLastName.getText().toString();
+        stringUserName = etUserName.getText().toString();
+        stringPassword = etPassword.getText().toString();
+        stringConPassword = etConPassword.getText().toString();
+        stringEmail = etEmail.getText().toString();
+
+        if(!stringFirstName.equals("") && !stringLastName.equals("")){
+            if(stringFirstName.matches("^[a-zA-Z ]*$") && stringLastName.matches("^[a-zA-Z ]*$")){
+                if(!stringPassword.equals("") && !stringConPassword.equals("")){
+                    if(stringPassword.equals(stringConPassword)){
+                        if(stringEmail.matches("^[a-z]{4}[0-9]{4}@student\\.monash\\.edu$")){
+                            Toast.makeText(this, "Registration Successful !!", Toast.LENGTH_LONG).show();
+                            saveToSharedPreference();
+                            switchLoginPage();
+                        }
+                        else{
+                            Toast.makeText(this, stringEmail, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    else{
+                        Toast.makeText(this, "Password and Confirm Password must be the same!!", Toast.LENGTH_LONG).show();
+
+                    }
+                }
+                else{
+                    Toast.makeText(this, "Please insert your password and confirm password!!", Toast.LENGTH_LONG).show();
+                }
+            }
+            else{
+                Toast.makeText(this, "Please insert your proper name!!", Toast.LENGTH_LONG).show();
+            }
+        }
+        else{
+            Toast.makeText(this, "Please insert your first name and last name!!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void saveToSharedPreference(){
+        SharedPreferences sharedPreferences = getSharedPreferences("USER_DATAFILE", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString("USERNAME", stringUserName);
+        editor.putString("PASSWORD", stringPassword);
+
+        editor.apply();
+    }
+
+    public void switchLoginPage(){
+        Intent loginIntent = new Intent(this, loginPage.class);
+        startActivity(loginIntent);
     }
 }
