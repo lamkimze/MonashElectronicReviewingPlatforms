39921c58eca6f11ab199f7562967db62838e0e3b
Jamie Le Tran <jtra0050@student.monash.edu>
2024-08-15 00:42:41 +1000

    Fully added the ability to select images from gallery and display them into recycler view.

--- app/src/main/java/com/example/myapplication/userReviewCreationPage.java
+++ app/src/main/java/com/example/myapplication/userReviewCreationPage.java
@@ -1,24 +1,98 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.Manifest;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class userReviewCreationPage extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    TextView textView;
+    ImageButton pick;
+
+    ArrayList<Uri> uri = new ArrayList<>();
+    RecyclerAdapter adapter;
+
+    public static final int Read_Permission = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_review_creation_page);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        textView = findViewById(R.id.totalPhotos);
+        recyclerView =findViewById(R.id.recyclerView_Gallery_Images);
+        pick = findViewById(R.id.imageButton);
+
+        adapter = new RecyclerAdapter(uri);
+        recyclerView.setLayoutManager(new GridLayoutManager(userReviewCreationPage.this,4));
+        recyclerView.setAdapter(adapter);
+
+        if(ContextCompat.checkSelfPermission(userReviewCreationPage.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(userReviewCreationPage.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, Read_Permission);
+        }
+
+        pick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                    intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+
+                }
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Picture"),1);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            if(data.getClipData() != null){
+                int count = data.getClipData().getItemCount();
+
+                for(int i = 0; i < count; i++){
+                    uri.add(data.getClipData().getItemAt(i).getUri());
+                }
+                adapter.notifyDataSetChanged();
+                textView.setText("Photos ("+uri.size()+")");
+
+            }else if(data.getData() != null){
+                String imageURL = data.getData().getPath();
+                uri.add(Uri.parse(imageURL));
+            }
+        }
     }
 }
