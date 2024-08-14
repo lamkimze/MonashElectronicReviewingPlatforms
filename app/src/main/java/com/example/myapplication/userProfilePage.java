76e06819f4a89c41f236dc767c84b61ec061992a
Rohan <rohan.sivam2562@gmail.com>
2024-08-14 14:21:27 +1000

    Created the user profile view as well as began to code both the profile views

--- app/src/main/java/com/example/myapplication/userProfilePage.java
+++ app/src/main/java/com/example/myapplication/userProfilePage.java
@@ -1,8 +1,7 @@
+
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.widget.RatingBar;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,23 +9,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class restaurantOwnerProfilePage extends AppCompatActivity {
-
-    TextView textView;
-    RatingBar ratingBar;
+public class userProfilePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_restaurant_owner_profile_page);
+        setContentView(R.layout.activity_user_profile_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        textView = (TextView) findViewById(R.id.textView17);
-        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
     }
 }
