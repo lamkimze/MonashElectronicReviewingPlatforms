3c876794112933dbc8d0e9388b3e5bb4e158b43b
Luke <lfer0019@student.monash.edu>
2024-09-04 11:05:59 +1000

    Added ImageType enum to differentiate a review image from a business image

---
+++ app/src/main/java/com/example/myapplication/Enumerables/ImageType.java
@@ -0,0 +1,24 @@
+package com.example.myapplication.Enumerables;
+
+public enum ImageType {
+    BUSINESS("business_image", "bus_id"),
+    REVIEW("review_image", "review_id");
+
+    private final String tableName;
+    private final String foreignKey;
+
+    ImageType(String tableName , String foreignKey) {
+        this.tableName = tableName;
+        this.foreignKey = foreignKey;
+    }
+
+    public String getTableName() {
+        return this.tableName;
+    }
+
+    public String getForeignKey() {
+        return this.foreignKey;
+    }
+
+
+}
