6f7f02baac7c077e59f03e17380e28b31fbebeff
Jamie Le Tran <jtra0050@student.monash.edu>
2024-09-11 22:37:40 +1000

    Created layout of favorites page for a user.

--- app/src/main/java/com/example/myapplication/favouriteRestaurantsPage.java
+++ app/src/main/java/com/example/myapplication/favouriteRestaurantsPage.java
@@ -1,20 +1,31 @@
 package com.example.myapplication;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.SearchView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class favouriteRestaurantsPage extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private List<Item> itemList;
+    private ItemAdapter itemAdapter;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_favourite_restaurants_page);
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
