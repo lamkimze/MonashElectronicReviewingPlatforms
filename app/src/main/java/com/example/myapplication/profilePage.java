8ab5532ea2384febcb513ac39d5f70aece1df495
Jamie Le Tran <jtra0050@student.monash.edu>
2024-09-12 13:01:17 +1000

    added round heart.

--- app/src/main/java/com/example/myapplication/profilePage.java
+++ app/src/main/java/com/example/myapplication/profilePage.java
@@ -1,10 +1,13 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
