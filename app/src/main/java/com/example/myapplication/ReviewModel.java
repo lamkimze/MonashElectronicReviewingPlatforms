19ff9c88e28b55cc74a8c2989eeec9906dd3a18b
klam0021 <klam0021@student.monash.edu>
2024-09-13 03:36:33 +1000

    create the restaurant detail page and added the functionalities

--- app/src/main/java/com/example/myapplication/ReviewModel.java
+++ app/src/main/java/com/example/myapplication/ReviewModel.java
@@ -5,12 +5,24 @@ import android.graphics.Bitmap;
 import java.util.ArrayList;
 
 public class ReviewModel {
+
+//    Kim
+    String uDp;
+    int likes, dislike;
+    int reviewId;
+    String position;
+    String timestamp;
+    ArrayList<ReviewModel> reply;
+    ArrayList<String> tags;
+    String pImage;
+
     String reviewTitle;
     int reviewRating;
     ArrayList<Bitmap> reviewImages;
     String reviewText;
     int reviewerId;
     int businessId;
+
 
     public ReviewModel(int reviewRating, String reviewTitle, ArrayList<Bitmap> reviewImages, String reviewText, int reviewerId, int businessId) {
         this.reviewRating = reviewRating;
@@ -43,5 +55,78 @@ public class ReviewModel {
 
     public int getBusinessId() {
         return businessId;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes += likes;
+    }
+
+    public int getDislike() {
+        return dislike;
+    }
+
+    public void setDislike(int dislike) {
+        this.dislike += dislike;
+    }
+
+    public void setPosition(String position){
+        this.position = position;
+    }
+
+    public String getPosition()
+    {
+        return this.position;
+    }
+
+    public void setTimestamp(String timestamp){
+        this.timestamp = timestamp;
+    }
+
+    public String getTimestamp(){
+        return timestamp;
+    }
+
+    public ArrayList<ReviewModel> getReply() {
+        return reply;
+    }
+
+    public void setReply(ArrayList<ReviewModel> reply) {
+        this.reply = reply;
+    }
+
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(ArrayList<String> tags) {
+        this.tags = tags;
+    }
+
+    public int getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(int reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public String getuDp() {
+        return uDp;
+    }
+
+    public void setuDp(String uDp) {
+        this.uDp = uDp;
+    }
+
+    public String getpImage() {
+        return pImage;
+    }
+
+    public void setpImage(String pImage) {
+        this.pImage = pImage;
     }
 }
--- app/src/main/java/com/example/myapplication/restaurantDetailPage.java
+++ app/src/main/java/com/example/myapplication/restaurantDetailPage.java
@@ -1,24 +1,160 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Pair;
+import android.util.Range;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.Database.CRUD_Business;
+import com.example.myapplication.Database.CRUD_Image;
+import com.example.myapplication.Database.CRUD_Review;
+import com.example.myapplication.Database.DatabaseHelper;
+import com.taufiqrahman.reviewratings.BarLabels;
+import com.taufiqrahman.reviewratings.RatingReviews;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class restaurantDetailPage extends AppCompatActivity {
+
+    int busId;
+    Restaurant selected_restaurant;
+    TextView restaurantName;
+    TextView tvCuisineType;
+    ImageView restaurantImage;
+    List<ReviewModel> postList = new ArrayList<>();
+    commentAdapter commentAdapter;
+    RecyclerView recyclerView;
+    DatabaseHelper dbHelper;
+    CRUD_Business crudBusiness;
+    CRUD_Review crudReview;
+    CRUD_Image crudImage;
+    String [] filters = {"5 Stars", "4 Stars", "3 Stars", "2 Stars", "1 Star"};
+    String [] sorts = {"Likes", "Reply", "Dislike"};
+    ArrayAdapter<String> filterAdapter;
+    ArrayAdapter<String> sortAdapter;
+    AutoCompleteTextView autoCompleteTextViewFilter, autoCompleteTextViewSort;
+    String sortOn;
+    ArrayList<String> filterOn = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_restaurant_detail_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        busId = getIntent().getExtras().getInt("busId");
+
+        loadData();
+        selected_restaurant = crudBusiness.getRestaurant(busId);
+        recyclerView = findViewById(R.id.commentRecyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        layoutManager.setStackFromEnd(true);
+        layoutManager.setReverseLayout(true);
+        recyclerView.setLayoutManager(layoutManager);
+        commentAdapter = new commentAdapter();
+        recyclerView.setAdapter(commentAdapter);
+
+        restaurantName = findViewById(R.id.detailRestaurantName);
+        tvCuisineType = findViewById(R.id.tvCuisineType);
+        restaurantImage = findViewById(R.id.detailRestaurantLogo);
+
+        restaurantName.setText(selected_restaurant.getName());
+        tvCuisineType.setText(selected_restaurant.getCuisine());
+
+        filterAdapter = new ArrayAdapter<>(this, R.layout.positions, filters);
+        sortAdapter = new ArrayAdapter<>(this, R.layout.positions, sorts);
+
+        autoCompleteTextViewFilter = findViewById(R.id.filter);
+        autoCompleteTextViewSort = findViewById(R.id.sort);
+
+        autoCompleteTextViewFilter.setAdapter(filterAdapter);
+        autoCompleteTextViewSort.setAdapter(sortAdapter);
+
+        autoCompleteTextViewSort.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                // Get the selected item
+                sortOn = adapterView.getItemAtPosition(position).toString();
+            }
         });
+
+//        autoCompleteTextViewFilter.setOnClickListener(new AdapterView.OnItemClickListener(){
+//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+//                filterOn =
+//            }
+//        });
+
+
+//        restaurantImage.setImageResource(crudImage.get);
+
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        RatingReviews ratingReviews = findViewById(R.id.rating_reviews);
+
+//        create color pairs for rating reviews
+        Pair color[] = new Pair[]{
+                new Pair<>(Color.parseColor("#0e9d58"), Color.parseColor("#1e88e5")),
+                new Pair<>(Color.parseColor("#bfd047"), Color.parseColor("#5c6bc0")),
+                new Pair<>(Color.parseColor("#ffc105"), Color.parseColor("#d81b68")),
+                new Pair<>(Color.parseColor("#ef7e14"), Color.parseColor("#8bc34a")),
+                new Pair<>(Color.parseColor("#d36259"), Color.parseColor("#ea80fc")),
+        };
+
+        int minValue = 30;
+
+//        create raters array
+        int raters[] = new int[]{
+                minValue + new Random().nextInt(100 - minValue + 1),
+                minValue + new Random().nextInt(100 - minValue + 1),
+                minValue + new Random().nextInt(100 - minValue + 1),
+                minValue + new Random().nextInt(100 - minValue + 1),
+                minValue + new Random().nextInt(100 - minValue + 1)
+        };
+
+        ratingReviews.createRatingBars(100, BarLabels.STYPE1, color, raters);
+        
     }
+
+    private void loadData() {
+        try{
+            dbHelper = new DatabaseHelper(this);
+            crudReview = new CRUD_Review(dbHelper);
+            crudBusiness = new CRUD_Business(dbHelper);
+            crudImage = new CRUD_Image(dbHelper);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void onClickCreateReview(View view){
+        Intent createReviewIntent = new Intent(this, userReviewCreationPage.class);
+        createReviewIntent.putExtra("busId", busId);
+        startActivity(createReviewIntent);
+    }
+
+    public void onClickBookmark(View view){
+    }
+
 }
