47691283e808926476123c45c5b5280c2c451f86
Jamie Le Tran <jtra0050@student.monash.edu>
2024-09-18 21:33:42 +1000

    Integrated the seekbar with the database such that the seekbars now display top 5 restaurants.

--- app/src/main/java/com/example/myapplication/CompetitionRecordsFragment.java
+++ app/src/main/java/com/example/myapplication/CompetitionRecordsFragment.java
@@ -9,13 +9,18 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.myapplication.Database.CRUD_Business;
+import com.example.myapplication.Database.CRUD_Image;
+import com.example.myapplication.Database.CRUD_Review;
+import com.example.myapplication.Database.CRUD_User;
 import com.example.myapplication.Database.DatabaseHelper;
 
 import java.util.ArrayList;
@@ -30,7 +35,6 @@ import java.util.TimerTask;
 public class CompetitionRecordsFragment extends Fragment {
 
     ArrayList<Restaurant> listRestaurant = new ArrayList<>();
-    DatabaseHelper dbHelper;
     FunctionalLibraries functionalLibraries = new FunctionalLibraries();
 
     // TODO: Rename parameter arguments, choose names that match
@@ -45,7 +49,26 @@ public class CompetitionRecordsFragment extends Fragment {
     TextView showCount1, showCount2, showCount3, showCount4, showCount5;
     SeekBar seekBar1, seekBar2, seekBar3, seekBar4, seekBar5;
     int counter1 = 0;
+    int counter2 = 0;
+    int counter3 = 0;
+    int counter4 = 0;
+    int counter5 = 0;
+
+
+    float businessNo1Rating;
+    float businessNo2Rating;
+    float businessNo3Rating;
+    float businessNo4Rating;
+    float businessNo5Rating;
     Handler handler = new Handler();
+
+    DatabaseHelper dbHelper;
+    CRUD_Business crudBusiness;
+    CRUD_User crudUser;
+    CRUD_Image crudImage;
+    CRUD_Review crudReview;
+
+    ArrayList<Restaurant> top5 = new ArrayList<>();
 
     public CompetitionRecordsFragment() {
         // Required empty public constructor
@@ -76,15 +99,15 @@ public class CompetitionRecordsFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-        try {
-            dbHelper = new DatabaseHelper(getContext());
-            CRUD_Business crudBusiness = new CRUD_Business(dbHelper);
-            ArrayList<Restaurant> dbRestaurants = crudBusiness.getAllRestaurants();
-            listRestaurant.addAll(dbRestaurants);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        initializeDatabase();
+//        try {
+//            dbHelper = new DatabaseHelper(getContext());
+//            CRUD_Business crudBusiness = new CRUD_Business(dbHelper);
+//            ArrayList<Restaurant> dbRestaurants = crudBusiness.getAllRestaurants();
+//            listRestaurant.addAll(dbRestaurants);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
         functionalLibraries.binarySort(listRestaurant, "restaurant");
     }
 
@@ -97,14 +120,20 @@ public class CompetitionRecordsFragment extends Fragment {
 
         seekBar1 = (SeekBar) fragmentView.findViewById(R.id.seekBar1);
         showCount1 = fragmentView.findViewById(R.id.top1Count);
-//        seekBar2 = (SeekBar) fragmentView.findViewById(R.id.seekBar2);
-//        showCount2 = fragmentView.findViewById(R.id.top2Count);
-//        seekBar3 = (SeekBar) fragmentView.findViewById(R.id.seekBar3);
-//        showCount3 = fragmentView.findViewById(R.id.top3Count);
-//        seekBar4 = (SeekBar) fragmentView.findViewById(R.id.seekBar4);
-//        showCount4 = fragmentView.findViewById(R.id.top4Count);
-//        seekBar5 = (SeekBar) fragmentView.findViewById(R.id.seekBar5);
-//        showCount5 = fragmentView.findViewById(R.id.top5Count);
+        seekBar2 = (SeekBar) fragmentView.findViewById(R.id.seekBar2);
+        showCount2 = fragmentView.findViewById(R.id.top2Count);
+        seekBar3 = (SeekBar) fragmentView.findViewById(R.id.seekBar3);
+        showCount3 = fragmentView.findViewById(R.id.top3Count);
+        seekBar4 = (SeekBar) fragmentView.findViewById(R.id.seekBar4);
+        showCount4 = fragmentView.findViewById(R.id.top4Count);
+        seekBar5 = (SeekBar) fragmentView.findViewById(R.id.seekBar5);
+        showCount5 = fragmentView.findViewById(R.id.top5Count);
+
+        seekBar1.setEnabled(false);
+        seekBar2.setEnabled(false);
+        seekBar3.setEnabled(false);
+        seekBar4.setEnabled(false);
+        seekBar5.setEnabled(false);
 
 
         Bitmap gygBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.guzmanygomez);
@@ -124,18 +153,139 @@ public class CompetitionRecordsFragment extends Fragment {
                     @Override
                     public void run() {
                         seekBar1.setProgress(counter1);
-                        showCount1.setText(String.valueOf(counter1));
+                        showCount1.setText(String.valueOf(counter1)+"%");
                     }
                 });
 
-                if(counter1 == 100){
+                if(counter1 > (businessNo1Rating/5)*100){
                     t.cancel();
                 }
             }
         };
+        Timer s = new Timer();
+        TimerTask ss = new TimerTask() {
+            @Override
+            public void run() {
+                counter2++;
+
+                // Update the UI on the main thread
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        seekBar2.setProgress(counter2);
+                        showCount2.setText(String.valueOf(counter2)+"%");
+                    }
+                });
+
+                if(counter2 > (businessNo2Rating/5)*100){
+                    s.cancel();
+                }
+            }
+        };
+        Timer no3t = new Timer();
+        TimerTask no3tt = new TimerTask() {
+            @Override
+            public void run() {
+                counter3++;
+
+                // Update the UI on the main thread
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        seekBar3.setProgress(counter3);
+                        showCount3.setText(String.valueOf(counter3)+"%");
+                    }
+                });
+
+                if(counter3 > (businessNo3Rating/5)*100){
+                    no3t.cancel();
+                }
+            }
+        };
+        Timer no4t = new Timer();
+        TimerTask no4tt = new TimerTask() {
+            @Override
+            public void run() {
+                counter4++;
+
+                // Update the UI on the main thread
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        seekBar4.setProgress(counter4);
+                        showCount4.setText(String.valueOf(counter4)+"%");
+                    }
+                });
+
+                if(counter4 > (businessNo4Rating/5)*100){
+                    no4t.cancel();
+                }
+            }
+        };
+        Timer no5t = new Timer();
+        TimerTask no5tt = new TimerTask() {
+            @Override
+            public void run() {
+                counter5++;
+
+                // Update the UI on the main thread
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        seekBar5.setProgress(counter5);
+                        showCount5.setText(String.valueOf(counter5)+"%");
+                    }
+                });
+
+                if(counter5 > (businessNo5Rating/5)*100){
+                    no5t.cancel();
+                }
+            }
+        };
         t.schedule(tt, 0, 100);
+        s.schedule(ss, 0, 100);
+        no3t.schedule(no3tt, 0, 100);
+        no4t.schedule(no4tt, 0, 100);
+        no5t.schedule(no5tt, 0, 100);
+
+
 
         return fragmentView;
+    }
+
+    private void initializeDatabase() {
+
+        try {
+            dbHelper = new DatabaseHelper(getContext());
+            crudBusiness = new CRUD_Business(dbHelper);
+            crudUser = new CRUD_User(dbHelper);
+            crudImage = new CRUD_Image(dbHelper);
+            crudReview = new CRUD_Review(dbHelper);
+            ArrayList<Restaurant> dashboardRestaurantsTop5 = crudBusiness.getTop5RatedBusinesses();
+            top5.addAll(dashboardRestaurantsTop5);
+
+            businessNo1Rating = crudBusiness.getBusinessStarRating(dashboardRestaurantsTop5.get(0));
+            businessNo2Rating = crudBusiness.getBusinessStarRating(dashboardRestaurantsTop5.get(1));
+            businessNo3Rating = crudBusiness.getBusinessStarRating(dashboardRestaurantsTop5.get(2));
+            businessNo4Rating = crudBusiness.getBusinessStarRating(dashboardRestaurantsTop5.get(3));
+            businessNo5Rating = crudBusiness.getBusinessStarRating(dashboardRestaurantsTop5.get(4));
+
+            ArrayList<Restaurant> dbRestaurants = crudBusiness.getAllRestaurants();
+            listRestaurant.addAll(dbRestaurants);
+
+            // Notify success on the main thread
+            new Handler(Looper.getMainLooper()).post(() ->
+                    Toast.makeText(getContext(), "Database initialized successfully!", Toast.LENGTH_LONG).show()
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            // Notify failure on the main thread
+            new Handler(Looper.getMainLooper()).post(() ->
+                    Toast.makeText(getContext(), "Database initialization failed!", Toast.LENGTH_LONG).show()
+            );
+        }
+
     }
 
 }
