45af038923e873fc425d380e15e62cc67ae9879e
Luke <lfer0019@student.monash.edu>
2024-09-25 11:26:17 +1000

    Added filter and search feature for restaurant list page

--- app/src/main/java/com/example/myapplication/restaurantListPage.java
+++ app/src/main/java/com/example/myapplication/restaurantListPage.java
@@ -2,8 +2,12 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.SearchView;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,20 +16,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.myapplication.Database.CRUD_Business;
-import com.example.myapplication.Database.CRUD_Business;
-import com.example.myapplication.Database.CRUD_User;
 import com.example.myapplication.Database.DatabaseHelper;
-import com.example.myapplication.RestaurantListAdapter;
+
 import java.util.ArrayList;
-import com.example.myapplication.Database.DatabaseHelper;
+import java.util.Comparator;
+import java.util.List;
 
 public class restaurantListPage extends AppCompatActivity {
 
 
-    RestaurantListAdapter restaurantListAdapter;
-    ArrayList<Restaurant> restaurants = new ArrayList<>();
-    ListView restaurantListView;
-    DatabaseHelper dbHelper;
+    private RestaurantListAdapter restaurantListAdapter;
+    private List<Restaurant> restaurants = new ArrayList<>();
+    private List<Restaurant> filteredRestaurants = new ArrayList<>();
+    private ListView restaurantListView;
+    private Spinner filterSpinner;
+    private SearchView searchView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,28 +44,54 @@ public class restaurantListPage extends AppCompatActivity {
             return insets;
         });
 
-    // Retrieve user_id from intent
+        //  Retrieve user_id from intent
         int userID = getIntent().getIntExtra("userID", 5);
 
-    // Initialize ListView and set the adapter
+        //  Initialize DatabaseHelper and CRUD_Business
+        DatabaseHelper dbHelper = new DatabaseHelper(this);
+        CRUD_Business crudBusiness = new CRUD_Business(dbHelper);
+
+        //  Initialize ListView and set the adapter
         restaurantListView = findViewById(R.id.restaurantListView);
-        restaurantListAdapter = new RestaurantListAdapter(this, restaurants);
+
+        // Initialize Spinner
+        filterSpinner = findViewById(R.id.filterSpinner);
+
+        // populate the spinner with the filter options
+        ArrayAdapter<CharSequence> filterAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.filter_options,
+                android.R.layout.simple_spinner_item
+        );
+
+        filterAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        filterSpinner.setAdapter(filterAdapter);
+
+        // Initialize SearchView
+        searchView = findViewById(R.id.searchView);
+
+
+        // Set the item click listener for the ListView
+        setListViewItemClickListener(userID);
+
+        // Set the item selected listener for the Spinner
+        setSpinnerItemSelectedListener();
+
+        // Set the query text listener for the SearchView
+        setSearchViewQueryTextListener();
+
+        // Initialize the RestaurantListAdapter
+        restaurantListAdapter = new RestaurantListAdapter(this, filteredRestaurants);
         restaurantListView.setAdapter(restaurantListAdapter);
 
-//        get restaurants from database
-        try {
-            dbHelper = new DatabaseHelper(this);
-            CRUD_Business crudBusiness = new CRUD_Business(dbHelper);
-            ArrayList<Restaurant> dbRestaurants = crudBusiness.getAllRestaurants();
-            restaurants.addAll(dbRestaurants);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        // Notify the adapter that the data has changed
+        // Get all restaurants from the database
+        restaurants.addAll(crudBusiness.getAllRestaurants());
+        filteredRestaurants.addAll(restaurants);
         restaurantListAdapter.notifyDataSetChanged();
 
-        // Set OnItemClickListener for the ListView
+    }
+
+    private void setListViewItemClickListener(int userID) {
         restaurantListView.setOnItemClickListener((parent, view, position, id) -> {
             Restaurant restaurant = restaurants.get(position);
             int businessID = restaurant.getId();
@@ -68,5 +100,74 @@ public class restaurantListPage extends AppCompatActivity {
             restuarantIntent.putExtra("busId", businessID);
             startActivity(restuarantIntent);
         });
+    }
+
+    private void setSpinnerItemSelectedListener() {
+        filterSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedFilter = parent.getItemAtPosition(position).toString();
+                // Apply filter based on selectedFilter
+                filterRestaurants(selectedFilter);
+                // Notify the adapter that the data has changed
+                restaurantListAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
+    }
+
+    private void filterRestaurants(String filter) {
+        List<Restaurant> listToFilter = new ArrayList<>(filteredRestaurants.isEmpty() ? restaurants : filteredRestaurants);
+        switch (filter) {
+            case "Top Rated":
+                // Sort restaurants by stars
+                listToFilter.sort((r1, r2) -> Float.compare(r2.getStars(), r1.getStars()));
+                break;
+            case "A-Z":
+                // Sort restaurants by name
+                listToFilter.sort(Comparator.comparing(Restaurant::getName));
+                break;
+            case "Z-A":
+                // Sort restaurants by name in reverse order
+                listToFilter.sort((r1, r2) -> r2.getName().compareTo(r1.getName()));
+                break;
+            default:
+                // Do nothing
+                break;
+        }
+        filteredRestaurants.clear();
+        filteredRestaurants.addAll(listToFilter);
+        restaurantListAdapter.notifyDataSetChanged();
+    }
+
+    private void setSearchViewQueryTextListener() {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                restaurantSearch(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                restaurantSearch(newText);
+                return true;
+            }
+        });
+    }
+
+    private void restaurantSearch(String query) {
+        filteredRestaurants.clear();
+        for (Restaurant restaurant : restaurants) {
+            if (restaurant.getName().toLowerCase().contains(query.toLowerCase())) {
+                filteredRestaurants.add(restaurant);
+            }
+        }
+        String selectedFilter = filterSpinner.getSelectedItem().toString();
+        filterRestaurants(selectedFilter);
     }
 }
