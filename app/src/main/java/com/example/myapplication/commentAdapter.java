6113ba713dc06a943c517929077f2ddd3640fba2
klam0021 <klam0021@student.monash.edu>
2024-09-30 15:08:28 +1000

    update crud on saving and updating likes and dislikes

--- app/src/main/java/com/example/myapplication/commentAdapter.java
+++ app/src/main/java/com/example/myapplication/commentAdapter.java
@@ -1,16 +1,21 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.text.format.DateFormat;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,6 +24,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.Database.CRUD_Image;
+import com.example.myapplication.Database.CRUD_Review;
 import com.example.myapplication.Database.CRUD_User;
 import com.example.myapplication.Database.DatabaseHelper;
 import com.example.myapplication.Entities.User;
@@ -38,8 +44,9 @@ public class commentAdapter extends RecyclerView.Adapter<commentAdapter.MyHolder
     List<ReviewModel> postList = new ArrayList<>();
     CRUD_User crudUser;
     CRUD_Image crudImage;
+    CRUD_Review crudReview;
     DatabaseHelper databaseHelper;
-    int busId;
+    int busId, userId;
 
     public commentAdapter() {
     }
@@ -61,6 +68,7 @@ public class commentAdapter extends RecyclerView.Adapter<commentAdapter.MyHolder
             databaseHelper = new DatabaseHelper(parent.getContext());
             crudUser = new CRUD_User(databaseHelper);
             crudImage = new CRUD_Image(databaseHelper);
+            crudReview = new CRUD_Review(databaseHelper);
         } catch (Exception e) {
             e.printStackTrace();
 
@@ -69,32 +77,19 @@ public class commentAdapter extends RecyclerView.Adapter<commentAdapter.MyHolder
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyHolder holder, @SuppressLint("RecyclerView") int position) {
         int uid = postList.get(position).getReviewerId();
         User currentUser = crudUser.getUser(uid);
         Bitmap profilePicture = currentUser.getProfilePicture();
-        String uDp = postList.get(position).getuDp();
         String uName = currentUser.getUsername();
-        String uEmail = currentUser.getEmail();
         Position userPosition = crudUser.getUserPoistion(uid);
-        String[] tag = postList.get(position).getTags();
         String pTitle = postList.get(position).getReviewTitle();
-        String pDescription = postList.get(position).getReviewText();
         String pImage = postList.get(position).getpImage();
         String postTime = postList.get(position).getTimestamp();
         float rating = postList.get(position).getReviewRating();
-
-        if(tag != null){
-            String itemTags = "";
-            for (int i = 0; i < tag.length; i++) {
-                itemTags += "#";
-                itemTags += tag[i];
-                itemTags += " ";
-            }
-            holder.itemTags.setText(itemTags);
-        }else{
-            holder.itemTags.setVisibility(View.GONE);
-        }
+        int commentNo = crudReview.getResponses(postList.get(position).reviewId).size();
+        int likesNo = postList.get(position).getLikes().size();
+        int disLikesNo = postList.get(position).getDislike().size();
 
 //        convert timestamp to dd/mm/yyyy hh:mm am/pm
         try {
@@ -125,16 +120,30 @@ public class commentAdapter extends RecyclerView.Adapter<commentAdapter.MyHolder
             throw new RuntimeException(e);
         }
 
-//        Calendar calendar = Calendar.getInstance(Locale.getDefault());
-//        calendar.setTimeInMillis(Long.parseLong(postTime));
-//        String pTime = DateFormat.format("dd/mm/yyyy hh:mm aa", calendar).toString();
-
 
 //        set Data
         holder.uNameTv.setText(uName);
         holder.pTitleTv.setText(pTitle);
-        holder.pDescriptionTv.setText(pDescription);
         holder.ratingBar.setRating(rating);
+        holder.likeBtn.setText(String.valueOf(likesNo) + " Likes ");
+        holder.commentBtn.setText(String.valueOf(commentNo) + " Comments ");
+        holder.disagreeBtn.setText(String.valueOf(disLikesNo) + " Dislikes ");
+
+        ArrayList<Integer> likeList = postList.get(position).getLikes();
+        ArrayList<Integer> disLikeList = postList.get(position).getDislike();
+
+        if (likeList.contains(userId)) {
+            holder.likeBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.thumb_up, 0, 0, 0); // Active like icon
+        } else {
+            holder.likeBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.unclicked_thumbs_up, 0, 0, 0); // Default like icon
+        }
+
+        if (disLikeList.contains(userId)) {
+            holder.disagreeBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.thumb_down, 0, 0, 0); // Active dislike icon
+        } else {
+            holder.disagreeBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.uncliked_thumbs_down, 0, 0, 0); // Default dislike icon
+        }
+
 
         if(profilePicture != null){
             Log.e("Profile Pic", String.valueOf(profilePicture));
@@ -142,55 +151,133 @@ public class commentAdapter extends RecyclerView.Adapter<commentAdapter.MyHolder
         }
 
         try{
-            if(userPosition.getBusinessID() == busId){
+            if(userPosition != null &&userPosition.getBusinessID() == busId){
                 holder.posTagTv.setText(userPosition.getPositionName());
             }else{
                 holder.posTagTv.setVisibility(View.GONE);
             }
         }catch (Exception e){
-
         }
 
-
-//        set post image
-        if(pImage == null){
-            holder.pImageIv.setVisibility(View.GONE);
-        }else{
-            try{
-                Picasso.get().load(pImage).into(holder.pImageIv);
-            }catch (Exception e){
-            }
-        }
 
 
 //        handle button click
         holder.moreBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(context, "More", Toast.LENGTH_SHORT).show();
+                showMoreOptions(holder.moreBtn, uid, userId, postList.get(position).getReviewId(), pImage);
             }
         });
 
         holder.likeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(context, "Like", Toast.LENGTH_SHORT).show();
+                ArrayList<Integer> likeList = postList.get(position).getLikes();
+                ArrayList<Integer> disLikeList = postList.get(position).getDislike();
+//                Log.e("likes inside", String.valueOf(likeList.contains(userId)));
+
+//                user liked the review and tap again
+                if(likeList.contains(userId)){
+                    likeList.remove(Integer.valueOf(userId));
+                    holder.likeBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.unclicked_thumbs_up, 0, 0, 0); // Default like icon
+                }else{
+                    likeList.add(userId);
+                    holder.likeBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.thumb_up, 0, 0, 0); // Active like icon
+                    if(disLikeList.contains(userId)){
+                        disLikeList.remove(Integer.valueOf(userId));
+                        holder.disagreeBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.uncliked_thumbs_down, 0, 0, 0); // Reset dislike button
+                    }
+                }
+                crudReview.updateLikes(postList.get(position).getReviewId(), likeList);
+                crudReview.updateDisLikes(postList.get(position).getReviewId(), disLikeList);
+                notifyDataSetChanged();
             }
         });
 
-        holder.commentBVtn.setOnClickListener(new View.OnClickListener() {
+        holder.commentBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(context, "Comment", Toast.LENGTH_SHORT).show();
+                Intent reviewDetailIntent = new Intent(context, samplePostDetailActivity.class);
+                reviewDetailIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                reviewDetailIntent.putExtra("busId", busId);
+                reviewDetailIntent.putExtra("userId", userId);
+                reviewDetailIntent.putExtra("reviewId", postList.get(position).getReviewId());
+                context.startActivity(reviewDetailIntent);
             }
         });
 
-        holder.shareBtn.setOnClickListener(new View.OnClickListener() {
+        holder.disagreeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(context, "Share", Toast.LENGTH_SHORT).show();
+                ArrayList<Integer> likeList = postList.get(position).getLikes();
+                ArrayList<Integer> disLikeList = postList.get(position).getDislike();
+//                Log.e("likes inside", String.valueOf(likeList.contains(userId)));
+
+//                user liked the review and tap again
+                if(disLikeList.contains(userId)){
+                    disLikeList.remove(Integer.valueOf(userId));
+                    holder.disagreeBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.uncliked_thumbs_down, 0, 0, 0); // Default like icon
+                }else{
+                    disLikeList.add(userId);
+                    holder.disagreeBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.thumb_down, 0, 0, 0); // Active like icon
+                    if(likeList.contains(userId)){
+                        likeList.remove(Integer.valueOf(userId));
+                        holder.likeBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.unclicked_thumbs_up, 0, 0, 0); // Reset dislike button
+                    }
+                }
+                crudReview.updateLikes(postList.get(position).getReviewId(), likeList);
+                crudReview.updateDisLikes(postList.get(position).getReviewId(), disLikeList);
+                notifyDataSetChanged();
             }
         });
+    }
+
+    private void setLikes(final MyHolder holder, final String postKey){
+    }
+
+    private void showMoreOptions(ImageButton moreBtn, int userId, int myId, final int pId, final String pImage){
+        PopupMenu popupMenu = new PopupMenu(context, moreBtn, Gravity.END);
+
+        if(userId == myId){
+            popupMenu.getMenu().add(Menu.NONE, 0, 0, "Delete");
+            popupMenu.getMenu().add(Menu.NONE, 1, 0, "Edit");
+
+        }
+        popupMenu.getMenu().add(Menu.NONE, 2, 0, "View Detail");
+
+        popupMenu.setOnMenuItemClickListener((menuItem -> {
+            int id = menuItem.getItemId();
+            if(id == 0){
+                beginDelete(pId);
+            }
+            else if(id == 1){
+                Intent intent = new Intent(context, userReviewCreationPage.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.putExtra("key", "editPost");
+                intent.putExtra("reviewId", pId);
+                intent.putExtra("busId", busId);
+                intent.putExtra("userId", userId);
+                context.startActivity(intent);
+            }
+            else if(id == 2){
+                Intent reviewDetailIntent = new Intent(context, samplePostDetailActivity.class);
+                reviewDetailIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                reviewDetailIntent.putExtra("busId", busId);
+                reviewDetailIntent.putExtra("userId", userId);
+                reviewDetailIntent.putExtra("reviewId", pId);
+                context.startActivity(reviewDetailIntent);
+            }
+            return false;
+        }));
+
+        popupMenu.show();
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private void beginDelete(int pId) {
+        crudReview.deleteReview(pId);
+        crudImage.deleteReviewImages(pId);
+        notifyDataSetChanged();
     }
 
     @Override
@@ -199,30 +286,25 @@ public class commentAdapter extends RecyclerView.Adapter<commentAdapter.MyHolder
     }
 
     class MyHolder extends RecyclerView.ViewHolder{
-        ImageView uPictureIv, pImageIv;
-        TextView uNameTv, pTimeTv, pTitleTv, pDescriptionTv, pLikesTv, posTagTv, itemTags;
+        ImageView uPictureIv;
+        TextView uNameTv, pTimeTv, pTitleTv, posTagTv;
         ImageButton moreBtn;
-        Button likeBtn, commentBVtn, shareBtn;
+        Button likeBtn, commentBtn, disagreeBtn;
         RatingBar ratingBar;
 
         public MyHolder(@NonNull View itemView) {
             super(itemView);
 
             uPictureIv = itemView.findViewById(R.id.uPictureIv);
-            pImageIv = itemView.findViewById(R.id.pImageTv);
             uNameTv = itemView.findViewById(R.id.uNameTv);
             pTimeTv = itemView.findViewById(R.id.pTimeTv);
             pTitleTv = itemView.findViewById(R.id.pTitleTv);
-            pDescriptionTv = itemView.findViewById(R.id.pDescriptionTv);
-            pLikesTv = itemView.findViewById(R.id.pLikesTv);
             moreBtn = itemView.findViewById(R.id.moreBtn);
             likeBtn = itemView.findViewById(R.id.agreeBtn);
-            commentBVtn = itemView.findViewById(R.id.commentBtn);
-            shareBtn = itemView.findViewById(R.id.disagreeBtn);
+            commentBtn = itemView.findViewById(R.id.commentBtn);
+            disagreeBtn = itemView.findViewById(R.id.disagreeBtn);
             ratingBar = itemView.findViewById(R.id.detailReviewRating);
             posTagTv = itemView.findViewById(R.id.positionTag);
-            itemTags = itemView.findViewById(R.id.itemTag);
-
         }
     }
 
